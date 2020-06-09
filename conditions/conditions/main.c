36f1edf033f5b1bd11de29b398672faef7cb3657
marie <marie.costa13@gmail.com>
2020-06-09 19:05:46 +0200

    creation du dossier conditions

---
+++ conditions/conditions/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  conditions
+//
+//  Created by Marie Costa on 09/06/2020.
+//  Copyright © 2020 Marie Costa. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
