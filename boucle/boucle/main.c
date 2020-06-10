d8f77474fd271da3f10b4bb788889aca5cb4c5b8
marie <marie.costa13@gmail.com>
2020-06-10 21:07:40 +0200

    Chapitre terminé

--- boucle/boucle/main.c
+++ boucle/boucle/main.c
@@ -12,7 +12,53 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     
+// BOUCLE WHILE
+    int nbreUser = 0;
+    while(nbreUser != 47) {
+        printf("Tappez le chiffre 47. \n");
+        scanf("%d", &nbreUser);
+    }
     
+    int compteur = 0;
+    
+    while(compteur <= 5) {
+        printf("La boucle vaut %d. \n", compteur);
+        compteur++;
+    }
+    /*
+     while(1) {
+        printf("Boucle infinie. \n");
+     }
+     */
+// ================================================================================================
+// ================================================================================================
+// ================================================================================================
+    
+// BOUCLE DO... WHILE
+    int doWhile = 0;
+    
+    do {
+        printf("Salut les dev' ! \n");
+        doWhile++;
+    } while(doWhile < 10);
+    // Si l'instruction est fausse, la boucle s'exécutera au moins une fois au lieu de s'arréter car le test se fait à la fin (pas le cas pour while).
+    
+// ================================================================================================
+// ================================================================================================
+// ================================================================================================
+    
+// BOUCLE FOR (avec cette boucle pas de risque de faire une boucle infinie par erreur)
+    // Pas besoin d'initialiser la variable de la boucle for
+    int forBoucle;
+    
+    // for(initialisation ; condition(doit être répétée ou non) ; incrémentation(décrémentation ou n'importe quel autre opération comme +=2 ava,cer de 2 en 2) )
+    for (forBoucle = 0; forBoucle <= 5; forBoucle++) {
+        printf("La boucle For vaut %d. \n", forBoucle);
+    }
+// ================================================================================================
+// ================================================================================================
+// ================================================================================================
+        
     printf("\n");
     return 0;
 }
