5e1537a20480421f0c54a782788a2e34896f0ee7
marie <marie.costa13@gmail.com>
2020-06-10 20:08:02 +0200

    Chapitre terminé

--- conditions/conditions/main.c
+++ conditions/conditions/main.c
@@ -37,9 +37,12 @@ int main(int argc, const char * argv[]) {
         } else {
             printf("Vous avez %d ans. Vous êtes majeur. \n", age);
         } */
-    //
+    // =============================================================================
+    // =============================================================================
+    // =============================================================================
     
     // BOOLEAN
+    // ========
         /*
         if (0) {
             printf("C'est vrai \n\n"); // Code wiil never be executed
@@ -63,28 +66,84 @@ int main(int argc, const char * argv[]) {
             int majeur2 = 1;
             int mineur2 = 0;
             
-            printf("Quel âge avez-vous ? \n");
+            printf("Quel âge avez-vous ? \n ");
             scanf("%d", &age2);
             
             if (age2 >= 18) {
-                printf("Boolean vaut %d. Vous êtes majeur. \n", majeur2);
+                printf("Boolean vaut %d. Vous êtes majeur. \n \n", majeur2);
             } else {
-                printf("Boolean vaut %d. Vous êtes mineur. \n", mineur2);
+                printf("Boolean vaut %d. Vous êtes mineur. \n \n", mineur2);
             }
-        //
+            
+            printf("\n");
+        // =============================================================================
+        // =============================================================================
+        // =============================================================================
     
-    
-        /*
-        int age = 0;
-        printf("Êtes vous majeur ? Quel age avez-vous ? \n");
-        scanf("%d", &age);
-        // Mineur ou majeur ?
-        if (age < 18) {
-            printf(false);
-        } else {
-            printf(true);
-        } */
-    
+    // SWITCH
+        int cheese = 0;
+        printf("Bonjour ! Combien de steak et de cheese voulez-vous dans votre burger ? Entre un et cinq. \n");
+        scanf("%d", &cheese);
+        
+        switch (cheese) {
+            case 1:
+                printf("Voici un cheese. Il est petit celui-là... \n \n");
+                break;
+            case 2:
+                printf("Voici un double cheese. Il est double ! \n \n");
+                break;
+            case 3:
+                printf("Voici un triple cheese. Très bon choix pour un estomac normal ! \n \n");
+                break;
+            case 4:
+                printf("Voici un quadruple cheese. Pour les estomacs affamés ! \n \n");
+                break;
+            case 5:
+                printf("Voici un quintuple cheese. Il est gros celui-ci... Vous devez avoir très faim ! \n");
+                break;
+                
+            default:
+                printf("Pas de cheese? Vous êtes sûr(e) ? \n \n");
+                break;
+        }
+        printf("\n \n");
+        
+        // Autre exemple
+            int menuMcdo;
+            printf("=== Menu mcdo === \n \n");
+            printf("1. Big Mac \n");
+            printf("2. Triple cheese \n");
+            printf("3. 280 \n");
+            printf("4. McWrap Poulet Bacon \n");
+            printf("5. CBO \n");
+            printf("Bonjour ! Avez-vous fait votre choix ? \n");
+            scanf("%d", &menuMcdo);
+            
+            switch (menuMcdo) {
+                case 1:
+                    printf("Un Big Mac? Très bien je note. \n");
+                    break;
+                case 2:
+                    printf("Un Triple cheese? Parfait je note. \n");
+                break;
+                case 3:
+                    printf("Un 280 ? Allez je note. \n");
+                    break;
+                case 4:
+                    printf("Un McWrap Poulet Bacon. \n");
+                    break;
+                case 5:
+                    printf("Un CBO. Voilà c'est noté. \n");
+                    break;
+                
+                    
+                default:
+                    printf("Vous ne voulez rien ? \n");
+                    break;
+            }
+        // =============================================================================
+        // =============================================================================
+        // =============================================================================    
     
     
     printf("\n");
