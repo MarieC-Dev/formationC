f475375fd89103437788016bc88c879d83c613e0
marie <marie.costa13@gmail.com>
2020-06-10 21:09:31 +0200

    TP : plus ou moins, premier jeu

---
+++ plusOuMoins-premierJeu/plusOuMoins-premierJeu/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  plusOuMoins-premierJeu
+//
+//  Created by Marie Costa on 10/06/2020.
+//  Copyright © 2020 Marie Costa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
