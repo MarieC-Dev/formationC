7d3e2a4248081016aa900f30dd660f55bc9d879b
marie <marie.costa13@gmail.com>
2020-06-11 22:05:04 +0200

    Début chapitre prog modulaire

---
+++ prog-modulaire/prog-modulaire/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  prog-modulaire
+//
+//  Created by Marie Costa on 11/06/2020.
+//  Copyright © 2020 Marie Costa. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+// Prototype pour annoncer une fonction
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
