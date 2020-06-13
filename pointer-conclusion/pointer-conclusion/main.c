bf7164b3bc4afdc4580f9f2b6634d0a10704e22f
marie <marie.costa13@gmail.com>
2020-06-13 22:29:03 +0200

    Début chapitre tableau

--- pointer-conclusion/pointer-conclusion/main.c
+++ pointer-conclusion/pointer-conclusion/main.c
@@ -7,9 +7,54 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+// EXEMPLE DE RESOLUTION DE PROBLEME POINTEUR (VOIR COURS)
+
+// PROTOTYPE, FONCTION, VARIABLE, POINTEUR (ne foncitonne pas)
+/*
+void decoupeMinutes(int heures, int minutes);
+
+int main(int argc, char *argv[])
+{
+    int heures = 0, minutes = 90;
+
+    decoupeMinutes(heures, minutes);
+
+    printf("%d heures et %d minutes", heures, minutes);
+
     return 0;
+}
+
+void decoupeMinutes(int heures, int minutes)
+{
+    heures = minutes / 60;  // 90 / 60 = 1
+    minutes = minutes % 60; // 90 % 60 = 30
+}
+*/
+//
+//
+//
+
+// PROTOTYPE, FONCTION, VARIABLE, POINTEUR (fonctionne)
+void cutMinutes(int *pointeurH, int *pointeurM);
+
+int main(int argc, char *argv[]) {
+    
+    int heures = 0, minutes = 0;
+    printf("Entrez un chiffre pour le convertir en durée \n");
+    scanf("%d", &minutes);
+    
+    // Envoyer l'adresse des heures et des minutes (var déclarées ci-dessus)
+    cutMinutes(&heures, &minutes);
+    
+    // Les valeurs (fonction) ont été modifiées
+    printf("%d heures et %d minutes. \n \n", heures, minutes);
+    
+    return 0;
+}
+
+void cutMinutes(int *pointeurH, int *pointeurM) {
+    *pointeurH = *pointeurM / 60;
+    *pointeurM = *pointeurM % 60;
 }
---
+++ tableau/tableau/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tableau
+//
+//  Created by Marie Costa on 13/06/2020.
+//  Copyright © 2020 Marie Costa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
