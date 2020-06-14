96de0d37a230825c136ae32253b303e6fbe1400b
marie <marie.costa13@gmail.com>
2020-06-14 11:07:50 +0200

    Début partie 2 : tableau

--- tableauFonction/tableauFonction/main.c
+++ tableauFonction/tableauFonction/main.c
@@ -7,9 +7,14 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
+    
+    
+    
+    printf("\n");
     return 0;
 }
