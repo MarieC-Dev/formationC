fc347fa651b98763fd3f147b3041b14e678a90dd
marie <marie.costa13@gmail.com>
2020-06-15 18:56:21 +0200

    Début du chapitre créer mes types de variables

---
+++ mes-types-de-variables/mes-types-de-variables/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  mes-types-de-variables
+//
+//  Created by Marie Costa on 15/06/2020.
+//  Copyright © 2020 bl. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
