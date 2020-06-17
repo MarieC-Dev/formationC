f042685916d9e0850371903bd0f8849d168f9547
marie <marie.costa13@gmail.com>
2020-06-17 21:36:41 +0200

    Début du chapitre ecrire et lire un fichier en C

---
+++ lire-ecrire-fichiersC/lire-ecrire-fichiersC/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  lire-ecrire-fichiersC
+//
+//  Created by Marie Costa on 17/06/2020.
+//  Copyright © 2020 bl. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// PROTOTYPE
+FILE* fopen(const char* nomDuFichier, const char* modeOuverture);
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("\n\n");
+    return 0;
+}
