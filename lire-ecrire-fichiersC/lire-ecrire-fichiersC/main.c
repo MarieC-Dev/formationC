c634fc53eea9cb703ba41ea4224c59a84d065877
marie <marie.costa13@gmail.com>
2020-06-18 09:17:27 +0200

    Chapitre en cours de lecture : fopen fclose ok

--- lire-ecrire-fichiersC/lire-ecrire-fichiersC/main.c
+++ lire-ecrire-fichiersC/lire-ecrire-fichiersC/main.c
@@ -11,10 +11,27 @@
 #include <string.h>
 
 // PROTOTYPE
+// Ouverture
 FILE* fopen(const char* nomDuFichier, const char* modeOuverture);
+// Fermeture
+int fclose(FILE* pointeurSurFichier);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    FILE* fichier = NULL;
+    
+    fichier = fopen("test.txt", "r+"); // paramètre : nom du fichier, r+ : lecture et écriture
+    
+    // Test d'ouverture du fichier (si non écrit le fichier n'existe pas)
+    if (fichier != NULL) {
+        printf("Possibilité d'écrire et de lire le fichier. \n");
+        // On peut lire et écrire dans le fichier
+        fclose(fichier);
+    } else {
+        printf("Impossible d'ouvrir le fichier test.txt. \n");
+    }
+    //
+    
     printf("\n\n");
     return 0;
 }
